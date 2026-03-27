405777367022768aed1a6d66c9d683b10ce7dbb0
Simret <simruineb@gmail.com>
2026-03-27 04:13:39 +0300

    Keycloak setup

--- frontend/js/auth-config.js
+++ frontend/js/auth-config.js
@@ -3,7 +3,7 @@
  * Used by index.html (login) and logout.html (Keycloak sign-out).
  */
 window.OAN_API_BASE = window.OAN_API_BASE || 'http://localhost:5001';
-window.OAN_KEYCLOAK_URL = window.OAN_KEYCLOAK_URL || 'http://localhost:8090';
+window.OAN_KEYCLOAK_URL = window.OAN_KEYCLOAK_URL || 'http://$EC2_HOST/auth';
 window.OAN_KEYCLOAK_REALM = window.OAN_KEYCLOAK_REALM || 'openagrinet';
 window.OAN_KEYCLOAK_CLIENT_ID = window.OAN_KEYCLOAK_CLIENT_ID || 'openagrinet-web';
 window.OAN_KEYCLOAK_BROWSER = window.OAN_KEYCLOAK_BROWSER !== false;
